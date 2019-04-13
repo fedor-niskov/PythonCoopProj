94cf13d5802dfcb94c032bedc5586d8823b7efb2
qeqer <macworlex@gmail.com>
2019-04-13 18:05:01 +0300

    Переделан класс Color, добавлены комментарии

--- kaleidoscope.py
+++ kaleidoscope.py
@@ -4,22 +4,24 @@ import random
 # стартовый цвет
 r, g, b = 150, 150, 150
 circle_size = 10
-color_dif = 30
 canv_size = 700
 
-# класс, обеспечивающий хранение цвета и выбор рандомного на основе текущего
+
 class Color():
-    def __init__(self):
-        self.r = 150
-        self.g = 150
-        self.b = 150
+    """Класс, обеспечивающий хранение цвета и выбор рандомного на основе текущего"""
+    def __init__(self, r = 150, g = 150, b = 150):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.color_dif = 30
     
-    def get_rand_color(self):
-        self.r = (random.randrange(-color_dif, color_dif) + r) % 206 + 50
-        self.g = (random.randrange(-color_dif, color_dif) + g) % 206 + 50
-        self.b = (random.randrange(-color_dif, color_dif) + b) % 206 + 50
-        res = "#" + "%0.2X" % self.r + "%0.2X" % self.g + "%0.2X" % self.b
-        return res
+    def randomize(self):
+        """Получить следующий рандомный цвет"""
+        self.r = (random.randrange(-self.color_dif, self.color_dif) + self.r) % 206 + 50
+        self.g = (random.randrange(-self.color_dif, self.color_dif) + self.g) % 206 + 50
+        self.b = (random.randrange(-self.color_dif, self.color_dif) + self.b) % 206 + 50
+        # print(self.r, self.g, self.b)
+        return self
 
     def get_code(self):
         res = "#" + "%0.2X" % self.r + "%0.2X" % self.g + "%0.2X" % self.b
@@ -29,29 +31,30 @@ class Color():
 
 
 class App(Tk):
+    """Главный класс приложения"""
     def __init__(self):
         super(App, self).__init__()
         self.geometry("{}x{}".format(canv_size, canv_size))
         self.title("Калейдоскоп")
         # создаем сам холст и помещаем его в окно
-        canv = Canvas(self, bg='white')
-        canv.grid(row = 0, column = 0, sticky = "wens")
+        self.canv = Canvas(self, bg='white')
+        self.canv.grid(row = 0, column = 0, sticky = "wens")
         # чтобы занимал все окно
         self.columnconfigure(index = 0, weight = 1)
         self.rowconfigure(index = 0, weight = 1)
         self.bind('<B1-Motion>', self.create_figure)
         
-        # добавялем меню с кнопкой очистки холста
+        # добавляем меню с кнопкой очистки холста
         main_menu = Menu(self)
-        main_menu.add_cascade(label="Очистить", command = lambda: canv.delete("all"))
+        main_menu.add_cascade(label="Очистить", command = lambda: self.canv.delete("all"))
         self.config(menu = main_menu)
 
         self.fig_type = "circle"
         # None в color_pick означает, что будет выбираться автоматически
         self.color_pick = None
         # стартовый цвет
+        self.color = Color()
         
-
         # добавить меню выбора цвета и меню выбора фигуры
 
         # центрируем окно по центру экрана
@@ -63,18 +66,24 @@ class App(Tk):
         self.mainloop()
         
     def create_figure(self, event):
+        """Метод, рисующий с отображением"""
         # тут можно реализовать переключение разных фигур с помощью self.fig_type
-        pass
+        
+        # изначальные координаты кружка
+        x1 = int(event.x) - circle_size
+        x2 = int(event.x) + circle_size
+        y1 = int(event.y) - circle_size
+        y2 = int(event.y) + circle_size
 
-    # функция для рандомного выбора цвета на основе предыдущего использованного
-    def get_rand_color():
-        global r, g, b
-        # +50 для убирания темных (черного)
-        r = (random.randrange(-color_dif, color_dif) + r) % 206 + 50
-        g = (random.randrange(-color_dif, color_dif) + g) % 206 + 50
-        b = (random.randrange(-color_dif, color_dif) + b) % 206 + 50
-        res = "#" + "%0.2X" % r + "%0.2X" % g + "%0.2X" % b
-        return res
+        x_s = self.winfo_width()
+        y_s = self.winfo_height()
+        color = self.color.get_code()
+        self.color.randomize()
+
+        # коэффициенты растяжения для отображения относительно диагоналей
+        x_k = y_s / x_s
+        y_k = x_s / y_s
+
 
 
 app = App()
