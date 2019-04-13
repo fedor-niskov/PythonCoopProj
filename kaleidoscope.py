7c3669e8499f6a3be48a821ff772987a2aba3734
qeqer <macworlex@gmail.com>
2019-04-13 17:33:25 +0300

    Color class and pick/randomize methods

--- kaleidoscope.py
+++ kaleidoscope.py
@@ -7,11 +7,32 @@ circle_size = 10
 color_dif = 30
 canv_size = 700
 
+# класс, обеспечивающий хранение цвета и выбор рандомного на основе текущего
+class Color():
+	def __init__(self):
+		self.r = 150
+		self.g = 150
+		self.b = 150
+	
+	def get_rand_color(self):
+		self.r = (random.randrange(-color_dif, color_dif) + r) % 206 + 50
+		self.g = (random.randrange(-color_dif, color_dif) + g) % 206 + 50
+		self.b = (random.randrange(-color_dif, color_dif) + b) % 206 + 50
+		res = "#" + "%0.2X" % self.r + "%0.2X" % self.g + "%0.2X" % self.b
+		return res
+
+	def get_code(self):
+		res = "#" + "%0.2X" % self.r + "%0.2X" % self.g + "%0.2X" % self.b
+		return res
+		
+
+
+
 class App(Tk):
 	def __init__(self):
 		super(App, self).__init__()
 		self.geometry("{}x{}".format(canv_size, canv_size))
-
+		self.title("Калейдоскоп")
 		# создаем сам холст и помещаем его в окно
 		canv = Canvas(self, bg='white')
 		canv.grid(row = 0, column = 0, sticky = "wens")
@@ -26,15 +47,18 @@ class App(Tk):
 		self.config(menu = main_menu)
 
 		self.fig_type = "circle"
-		# None в color означает, что будет выбираться автоматически
-		self.color = None
+		# None в color_pick означает, что будет выбираться автоматически
+		self.color_pick = None
+		# стартовый цвет
+		
+
 		# добавить меню выбора цвета и меню выбора фигуры
 
 		# центрируем окно по центру экрана
-		root.update_idletasks()
-		x = (root.winfo_screenwidth() - root.winfo_width()) / 2
-		y = (root.winfo_screenheight() - root.winfo_height()) / 2
-		root.wm_geometry("+%d+%d" % (x, y))
+		self.update_idletasks()
+		x = (self.winfo_screenwidth() - self.winfo_width()) / 2
+		y = (self.winfo_screenheight() - self.winfo_height()) / 2
+		self.wm_geometry("+%d+%d" % (x, y))
 
 		self.mainloop()
 		
@@ -42,6 +66,15 @@ class App(Tk):
 		# тут можно реализовать переключение разных фигур с помощью self.fig_type
 		pass
 
+	# функция для рандомного выбора цвета на основе предыдущего использованного
+	def get_rand_color():
+		global r, g, b
+		# +50 для убирания темных (черного)
+		r = (random.randrange(-color_dif, color_dif) + r) % 206 + 50
+		g = (random.randrange(-color_dif, color_dif) + g) % 206 + 50
+		b = (random.randrange(-color_dif, color_dif) + b) % 206 + 50
+		res = "#" + "%0.2X" % r + "%0.2X" % g + "%0.2X" % b
+		return res
 
 
 app = App()
